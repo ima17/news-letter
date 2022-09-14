d957ab55d16f92671ff1a2085bfa43ede41808e5
ima17 <ima.sudaraka@outlook.com>
2022-09-14 23:22:14 +0530

    Setup the frontend

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static("public"));
+
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    var fName = req.body.fName;
+    var lName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(fName+ lName+ email);
 });
 
 app.listen(3000,function(){
