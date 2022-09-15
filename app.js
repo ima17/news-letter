85b573de69268eadc780146b33caa80014c5831c
ima17 <ima.sudaraka@outlook.com>
2022-09-15 22:42:30 +0530

    Connected to the server

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",function(req,res){
-    var fName = req.body.fName;
-    var lName = req.body.lName;
-    var email = req.body.email;
+app.post("/", function (req, res) {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const email = req.body.email;
 
-    console.log(fName+ lName+ email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us9.api.mailchimp.com/3.0/lists/5345ea52a3";
+  const options = {
+    method: "POST",
+    auth: "ima17:18fd6efa2a27ecaef2e07cbcd8a9f2be-us9",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
+
+
